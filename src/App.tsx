6cc30440ec5f142df6d683d95a800fd1c0e80935
antoniocarlosrs <antoniocarlosrs.dev@gmail.com>
2024-12-01 22:43:22 -0300

    Inicio

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from './App.module.css'
+import logoImg from './assets/Logo1.png'
+
+const App = () => {
+  return (
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={logoImg} alt="Imagem de logo" width={180} />
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App

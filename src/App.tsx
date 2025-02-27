b8092046441e64496d88809c7f39a285731f85b1
antoniocarlosrs <antoniocarlosrs.dev@gmail.com>
2025-02-26 22:03:24 -0300

    Criação lado direito

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import styles from './App.module.css'
-import logoImg from './assets/Logo1.png'
+import { useState } from "react";
+import styles from "./App.module.css";
+import logoImg from "./assets/Logo1.png";
 
 const App = () => {
+  const [heightFild, setHeightField] = useState<number>(0);
+  const [weighttFild, setWeightField] = useState<number>(0);
+
+  // Função calcular
+  const handleCalculateButton = () => {
+    if (heightFild && weighttFild) {
+    
+    } else {
+      alert('Digite todos os campos.');
+    }
+  }
+
   return (
     <div className={styles.main}>
       <header>
@@ -12,15 +25,38 @@ const App = () => {
 
       <div className={styles.container}>
         <div className={styles.lefSide}>
-          ...
+          <h1>Calcule seu IMC</h1>
+          <p>
+            IMC é a sigla para Índice de Massa Corporal, uma fórmula que calcula
+            a relação entre o peso e a altura de uma pessoa. É uma das formas de
+            avaliação corporal recomendadas pela Organização Mundial de Saúde
+            OMS.{" "}
+          </p>
+
+          <input
+            type="number"
+            placeholder="Digite sua altura"
+            value={heightFild > 0 ? heightFild : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+
+          <input
+            type="number"
+            placeholder="Digite seu peso"
+            value={weighttFild > 0 ? weighttFild : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+
         </div>
 
         <div className={styles.rightSide}>
-          ...
+        ...
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
